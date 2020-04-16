6f494a920ad2a0ce753a60b6a2fe04f8d51db97a
slavchikpy <59263833+slavchikpy@users.noreply.github.com>
2020-04-16 17:55:53 +0300

    Update main.go fo ht1-ht5

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"./ht1"
 
 	"./ht2"
+
+	"./ht3"
+
+	"./ht4"
+
+	"./ht5"
 )
 
 func main() {
@@ -13,4 +19,10 @@ func main() {
 
 	endPoint, perimeter, area := ht2.Call()
 	fmt.Println("Home task 2:\n\tendPoint=", endPoint, "\n\tperimeter=", perimeter, "\n\tarea=", area)
+
+	fmt.Println("Home task 3: ", ht3.Call([6]int{0, 3, 2, 7, 16, 5}))
+
+	fmt.Println("Home task 4:\n\tmax:", ht4.CallMax([]string{"one", "two"}), "\n\treverse:", ht4.CallReverse([]int64{1, 2, 5, 15}))
+
+	fmt.Println("Home task 5: ", ht5.Call(map[int]string{10: "aa", 0: "bb", 500: "cc"}))
 }
